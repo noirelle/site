1fa425117ee6ac3d19d0a77b4c314a73a84092e0
Noirelle <nouralivre@gmail.com>
2025-02-13 14:20:46 +0700

    iro-iro

--- sync.sh
+++ sync.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-rsync -av --delete --exclude=".*" ~/storage/documents/Obsidian/Medium/ content
+rsync -a --delete --exclude=".*" ~/storage/documents/Obsidian/Medium/ content
+
+rsync -a --remove-source-files --delete content/media assets
+rmdir content/media
+
 mv content/20250211-01.md content/_index.md
